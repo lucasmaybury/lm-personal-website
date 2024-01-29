b54bfd251695ceec922fc93a962960f67a893aa8
Lucas <>
2024-01-29 21:00:13 +0800

    add title and some responsive design

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -1,5 +1,9 @@
 export default function Navbar() {
-    return <nav>
-        <span>nav text</span>
-    </nav>
+    return (
+        <nav>
+            <div className="h-16 bg-gray-200 shadow-lg">
+                <span>nav text</span>
+            </div>
+        </nav>
+    )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,34 @@
+"use client";
+
 import Image from 'next/image'
+import useWindowDimensions from './utils/useWindowDimensions';
 import Sidebar from './sidebar';
 
 export default function Home() {
+  const { width } = useWindowDimensions();
   return (
     <>
-      <Sidebar/>
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <div id='profile' className="w-72 m-6">
+      {/* <Sidebar/> */}
+      <main className="flex flex-col items-center justify-between p-7 lg:p-24">
+        <div className="mb-4 mx-4">
+          <span>
+            <h1>Lucas Maybury</h1>
+            <h2>Fullstack Software Engineer</h2>
+          </span>
+        </div>
+        
+
+        <div id='profile' className="m-4">
           <Image 
             src='/profile-pic.jpg' 
-            width={300}
-            height={300}
+            width={width > 800 ? 300 : 200 }
+            height={width > 800 ? 300 : 200}
             alt="Picture of the author"
             className='rounded-full'
           />
         </div>
-        <div id='personal-statement'>
+
+        <div id='personal-statement' className="m-4 md:max-w-[32rem]">
           <span >
               A professional full-stack developer with an eye for design. Communicative and blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah
           </span>
